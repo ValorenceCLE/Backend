19feb4ff87b4b530696f68632a75329a424955d0
ValorenceCLE <landon.bell@valorence.com>
2025-03-06 09:45:58 -0700

    fix

--- app/utils/config.py
+++ app/utils/config.py
@@ -1,21 +1,8 @@
 from pydantic_settings import BaseSettings
 from pathlib import Path
-import os
-from dotenv import load_dotenv
 
-if os.environ.get("ENV_FILE"):
-    ENV_PATH = Path(os.environ.get("ENV_FILE"))
-elif os.path.exists('/.dockerenv'):
-    ENV_PATH = Path('/settings.env')
-else:
-    BASE_DIR = Path(__file__).resolve().parents[2]
-    ENV_PATH = BASE_DIR / 'secrets' / 'settings.env'
-
-if not ENV_PATH.exists():
-    raise FileNotFoundError(f"Environment file not found at: {ENV_PATH}")
-
-# Optionally, load environment variables into os.environ
-load_dotenv(dotenv_path=ENV_PATH)
+BASE_DIR = Path('/app')
+ENV_PATH = BASE_DIR / 'settings.env'
 
 class Settings(BaseSettings):
     APP_NAME: str = 'FastAPI App'
@@ -30,7 +17,7 @@ class Settings(BaseSettings):
     HASHED_ADMIN_PASSWORD: str
 
     class Config:
-        env_file = str(ENV_PATH)
+        env_file = str('/settings.env')
         env_file_encoding = 'utf-8'
         extra = 'ignore'
 
