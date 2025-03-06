708d362503853b4082f3822efbaf7051b7b48708
ValorenceCLE <landon.bell@valorence.com>
2025-03-05 17:29:10 -0700

    touches

--- app/main.py
+++ app/main.py
@@ -32,7 +32,7 @@ app = FastAPI(title=settings.APP_NAME, lifespan=lifespan)
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "http://localhost:80"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
